7a8dca6856e7fab2da8edae60bbd4a427c7a33e1
Zokerus <Zokerus@users.noreply.github.com>
2023-07-18 17:44:50 +0300

    Created design of dialogue editor
    Created layout of dialogue editor
    - added graph editor
    - added graph nodes as scenes
    - added menu buttons of dialogue editor

---
+++ DialogueNodes/GNode.gd
@@ -0,0 +1,5 @@
+extends GraphNode
+class_name GNode
+
+func _on_resize_request(new_minsize):
+	size = new_minsize
---
+++ DialogueNodes/OptionsNode.gd
@@ -0,0 +1,12 @@
+extends GNode
+
+var number_of_options : int = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_slot(0, true, 0, Color(1,1,1,1), true, 0, Color(1,1,1,1))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

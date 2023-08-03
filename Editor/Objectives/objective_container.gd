6ae31e96e0c829ef4660c1f42db4880e30a0d70b
Zokerus <Zokerus@users.noreply.github.com>
2023-08-03 08:13:01 +0300

    Added functions to objective template
    - added set and get functions to the fight/kill objective template
    - fixed objective template change by loading data

--- Editor/Objectives/objective_container.gd
+++ Editor/Objectives/objective_container.gd
@@ -31,11 +31,11 @@ func _on_option_button_item_selected(index):
 		
 	if objective_templates.has(option_button.get_item_text(index)):	## Match optionbutton selection to enum
 		template = objective_templates[option_button.get_item_text(index)].instantiate()	## load and instantiate matching template
+		v_box_container.add_child(template)	## add template to tree
 		if template.has_signal("press_edit_dialogue"):
 			template.press_edit_dialogue.connect(_on_press_edit_dialogue)
 		if option_button.get_item_text(index) == OBJECTIVE_TYPES.keys()[OBJECTIVE_TYPES.Fight]:	## in case of kill quest, use fight template and set the "kill" boolean to true
 			template.kill = true
-		v_box_container.add_child(template)	## add template to tree
 
 func _on_button_pressed():	## close the objective 
 	queue_free()
@@ -52,4 +52,5 @@ func get_objective_data() -> Dictionary:
 
 func set_quest_data(objective_data):
 	option_button.select(option_button.get_item_index(OBJECTIVE_TYPES[objective_data["type"]]))
+	option_button.item_selected.emit(option_button.get_item_index(OBJECTIVE_TYPES[objective_data["type"]]))
 	template.set_data(objective_data["data"])
--- Editor/Objectives/template_fight.gd
+++ Editor/Objectives/template_fight.gd
@@ -1,6 +1,8 @@
 extends BoxContainer
 
 @onready var target = $HBoxContainer/Target
+@onready var target_type = $HBoxContainer2/TargetType
+
 
 var kill : bool = false
 
@@ -19,3 +21,18 @@ func update_option_target(list: Dictionary):
 	target.clear()
 	for item in list:
 		target.add_item(list[item].name, item)
+
+func get_data() -> Dictionary:
+	var data: Dictionary
+	data["target_type"] = target_type.get_item_text(target_type.selected)
+	data["id"] = target.get_selected_id()
+	data["kill"] = kill
+	return data
+
+func set_data(data: Dictionary):
+	if data["target_type"] == "NPC":
+		target_type.select(0)
+	else :
+		target_type.select(1)
+	target.select(target.get_item_index(data["id"]))
+	kill = data["kill"]
